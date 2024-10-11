ad1ef8690e1d395573c2224dac955016dca58ce1
root <root@ip-172-31-39-63.ec2.internal>
2024-10-11 10:01:12 +0000

    model updated4

--- myapp/models.py
+++ myapp/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=100, blank=True)
     birth_date = models.DateField(null=True, blank=True)
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    #phone_number = models.CharField(max_length=15, blank=True, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now=True)
